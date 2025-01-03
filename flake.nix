4d21d9863cae0ad631ef75f062d33542a81da824
cat-phish <134035929+cat-phish@users.noreply.github.com>
2025-01-02 21:01:24 -0800

    I use Nix now btw

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "My Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = {
+        jordans-desktop = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            ./hosts/desktop/configuration-desktop.nix
+          ];
+          specialArgs = {
+            inherit pkgs-unstable;
+          };
+        };
+        jordans-laptop = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            ./hosts/laptop/configuration-laptop.nix
+          ];
+          specialArgs = {
+            inherit pkgs-unstable;
+          };
+        };
+      };
+      homeConfigurations = {
+        "jordan@jordans-desktop" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home.nix
+            ./hosts/desktop/home-desktop.nix
+          ];
+          extraSpecialArgs = {
+            inherit pkgs-unstable;
+          };
+        };
+        "jordan@jordans-laptop" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home.nix
+            ./hosts/laptop/home-laptop.nix
+          ];
+          extraSpecialArgs = {
+            inherit pkgs-unstable;
+          };
+        };
+      };
+    };
+}
