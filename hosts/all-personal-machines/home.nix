ede0b67967d0fadcf07b2de044450b889ba5c498
cat-phish <134035929+cat-phish@users.noreply.github.com>
2025-01-08 16:12:44 -0800

    added linking of theme files to .local/share

--- hosts/all-personal-machines/home.nix
+++ hosts/all-personal-machines/home.nix
@@ -37,8 +37,42 @@
     ]);
 
   home.file = {
+    ".wezterm.lua" = {
+      source = ../../dotfiles/.wezterm.lua;
+    };
+
     "${config.xdg.configHome}/kmonad" = {
       source = ../../dotfiles/.config/kmonad;
+      recursive = true;
+    };
+
+    # Theming
+    ".local/share/icons/Memphis98" = {
+      source = ../../dotfiles/.local/share/icons/Memphis98;
+      recursive = true;
+    };
+    ".local/share/icons/Reactionary" = {
+      source = ../../dotfiles/.local/share/icons/Reactionary;
+      recursive = true;
+    };
+
+    ".local/share/plasma/desktoptheme/reactplus" = {
+      source = ../../dotfiles/.local/share/plasma/desktoptheme/reactplus;
+      recursive = true;
+    };
+
+    ".local/share/plasma/look-and-feel/org.magpie.reactplus.desktop" = {
+      source = ../../dotfiles/.local/share/plasma/look-and-feel/org.magpie.reactplus.desktop;
+      recursive = true;
+    };
+
+    ".local/share/plasma/desktoptheme/reactionary" = {
+      source = ../../dotfiles/.local/share/plasma/desktoptheme/reactionary;
+      recursive = true;
+    };
+
+    ".local/share/plasma/look-and-feel/Reactionary" = {
+      source = ../../dotfiles/.local/share/plasma/look-and-feel/Reactionary;
       recursive = true;
     };
   };
