c39ed3cf3b805688f344420f6e881ad4b62d9e83
cat-phish <134035929+cat-phish@users.noreply.github.com>
2025-11-20 12:32:35 -0800

    Add Fedora Home Manager option to init script
    Updated init.sh to include Fedora Home Manager option and conditional execution for NixOS rebuild and Home Manager switch commands.

--- init.sh
+++ init.sh
@@ -3,8 +3,9 @@
 #cd ~/.nix || echo "~/.nix does not exist. Exiting" && exit 1
 
 echo "Please choose the host type:"
-echo "1) Desktop"
-echo "2) Laptop"
+echo "1) Desktop NixOS"
+echo "2) Laptop NixOS"
+echo "3) Fedora Home Manager"
 read -p "Enter the number of your choice: " choice
 
 case $choice in
@@ -18,13 +19,18 @@ case $choice in
         nixos_flake="jordans-laptop"
         home_manager_flake="jordan@jordans-laptop"
         ;;
+      3)
+        host_type="desktop"
+        home_manager_flake="jordan@fedora-desktop"
     *)
         echo "Invalid choice. Exiting."
         exit 1
         ;;
 esac
 
-cp /etc/nixos/hardware-configuration.nix ~/.nix/hosts/$host_type/hardware-configuration.nix
+if [ "$choice" = 1 ] || [ "$choice" = 2 ]; then
+  cp /etc/nixos/hardware-configuration.nix ~/.nix/hosts/$host_type/hardware-configuration.nix
+fi
 
 mkdir -p ~/.local/share
 cp -r ~/.nix/dotfiles/.local/share/* ~/.local/share/
@@ -35,8 +41,20 @@ cp -r ~/.nix/dotfiles/.icons/* ~/.icons/
 mkdir -p ~/.themes
 cp -r ~/.nix/dotfiles/.themes/* ~/.themes/
 
-sudo nixos-rebuild switch --flake .#$nixos_flake
-# sudo nixos-rebuild switch --flake .
+if [ "$choice" = 1 ] || [ "$choice" = 2 ]; then
+  sudo nixos-rebuild switch --flake .#$nixos_flake
+fi
 
-home-manager switch --flake .#$home_manager_flake
-# home-manager switch --flake .
+# TODO: make i this check if nix is installed already
+if [ "$choice" = 1 ] || [ "$choice" = 2 ]; then
+  home-manager switch --flake .#$home_manager_flake
+elif [ "$choice" = 3]; then
+  sudo dnf copr enable petersen/nix
+  sudo dnf install nix
+  sudo systemctl enable --now nix-daemon
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  nix-channel --update
+  nix-shell '<home-manager>' -A install
+  source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+  home-manager switch -b backup --flake .#$home_manager_flake
+fi
