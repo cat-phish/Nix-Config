99ff80b8b69931431c5b75d449f65ae806e2e80a
cat-phish <134035929+cat-phish@users.noreply.github.com>
2025-12-21 13:09:00 -0800

    updated fedora-desktop host

--- hosts/fedora-desktop-home.nix
+++ hosts/fedora-desktop-home.nix
@@ -9,10 +9,23 @@
   # Plasma Manager KDE Configuration
   imports = [
     # ../modules/plasma-config/desktop/plasma-config.nix
-    ../modules/rclone/rclone-gdrivedesk.nix
-    ../modules/rclone/rclone-mediaserversmb.nix
-    ../modules/rclone/rclone-hetzner.nix
+    # ../../modules/plasma-config/desktop/plasma-config.nix
+    # ../modules/rclone/rclone-gdrivedesk.nix
+    # ../modules/rclone/rclone-mediaserversmb.nix
+    # ../modules/rclone/rclone-hetzner.nix
     ../modules/home/app-beets.nix
+    ../modules/home/communications.nix
+    ../modules/home/dev-environment.nix
+    ../modules/home/fun-apps.nix
+    ../modules/home/media-playback.nix
+    ../modules/home/media-mgmt.nix
+    ../modules/home/office-suite.nix
+    ../modules/home/utilities.nix
+    ../modules/home/app-wine.nix
+    ../modules/home/app-foobar2000-wine-dependencies.nix
+    ../modules/home/app-wireguard-tray.nix
+    # ../modules/home/app-tailscale.nix
+    # inputs.sops-nix.homeManagerModules.sops
     # inputs.sops-nix.homeManagerModules.sops
   ];
 
@@ -20,6 +33,11 @@
   # beets_acoustid_api = {};
   # env_file = {};
   # };
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "jordan";
+  home.homeDirectory = "/home/jordan";
 
   home.packages =
     (with pkgs; [
@@ -131,8 +149,31 @@
 
   home.file = {
     # ".ssh/.env".text = "cat ${config.sops.secrets."env_file".path}";
+    ".scripts" = {
+      source = ../dotfiles/.scripts;
+      recursive = true;
+    };
+    ".python-scripts" = {
+      source = ../dotfiles/.python-scripts;
+      recursive = true;
+    };
     ".config/niri/config.kdl" = {
       source = ../dotfiles/.config/niri/config.kdl;
     };
+    "${config.xdg.configHome}/kmonad" = {
+      source = ../dotfiles/.config/kmonad;
+      recursive = true;
+    };
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    SHELL = "/usr/bin/zsh";
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
   };
 }
