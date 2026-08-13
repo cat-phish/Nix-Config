8da2cea0215b50029cc87f8b631b849b8b3781ac
cat-phish <134035929+cat-phish@users.noreply.github.com>
2026-08-12 18:49:49 -0700

    update

--- modules/home/games.nix
+++ modules/home/games.nix
@@ -17,6 +17,22 @@
     (with pkgs; [
       steam
       zenity
+
+      # Standalone Emulators
+      dolphin-emu
+      pcsx2
+      rpcs3
+
+      # RetroArch with specific libretro cores baked in
+      (retroarch.override {
+        cores = with libretro; [
+          snes9x
+          mupen64plus
+          nestopia
+          pcsx-rearmed
+          genesis-plus-gx
+        ];
+      })
     ])
     ++ (with pkgs-stable; [
       ]);
